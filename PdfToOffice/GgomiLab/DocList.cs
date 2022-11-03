81b2038880209e24b42126c69876117cb2a55b9b
Bundo Lee <ailnk@naver.com>
2022-11-04 02:44:48 +0900

    [GgomiLab] #1 사용자는 변환하길 원하는 파일을 선택할 수 있다
    [내용]
    - GgomiLab 프로젝트 생성
    - 코딩 가이드 및 실험적 기능을 위한 프로젝트
    - 사용자가 원하는 파일을 선택할 수 있는 기능 구현
    - 파일 탐색기 스타일의 UX 적용
    - Vector 아이콘 적용
    - MVVM 디자인 패턴 적용
    - 데이터 바인딩으로 ListBox와 ViewModel이 TwoWay로 데이터를 공유한다.
    - Commad 패턴으로 View에 입력이 들어오면 ViewModel에 전달한다.
    
    [영향 범위]
    - GgomiLab

---
+++ PdfToOffice/GgomiLab/DocList.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+
+namespace GgomiLab
+{
+    public class DocList : ObservableCollection<Doc>
+    {
+        public DocList()
+        {
+            Add(new Doc("C:/First PDF.pdf"));
+            Add(new Doc("C:/Second PDF.pdf"));
+            Add(new Doc("C:/Third PDF.pdf"));
+            Add(new Doc("C:/Fourth PDF.pdf"));
+            Add(new Doc("C:/Fifth PDF.pdf"));
+        }
+    }
+}
---
+++ PdfToOffice/GgomiLab/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Win32;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+
+namespace GgomiLab
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            AddCommandBindings(ApplicationCommands.Open, OpenCommandHandler);
+        }
+
+        private void OpenCommandHandler(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            bool? result = dialog.ShowDialog();
+            if (result == true)
+            {
+                string fileName = dialog.FileName;
+                ICollection<Doc> itemsSource = (ICollection<Doc>)IDC_DocList.ItemsSource;
+                if (itemsSource != null)
+                {
+                    itemsSource.Add(new Doc(fileName));
+                }
+            }
+        }
+
+        private void AddCommandBindings(ICommand command, ExecutedRoutedEventHandler handler)
+        {
+            var commandBinding = new CommandBinding(command, handler);
+            CommandBindings.Add(commandBinding);
+        }
+    }
+}
