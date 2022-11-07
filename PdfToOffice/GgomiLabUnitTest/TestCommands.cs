693af0b1831ac95c85d9d5d99b83d1aa85bef529
Bundo Lee <ailnk@naver.com>
2022-11-07 13:00:54 +0900

    [GgomiLab] 단위테스트 추가
    [내용]
    - DocListBox.AddDoc Command를 테스트할 수 있는 단위테스트 추가
    
    [영향]
    - 단위테스트

---
+++ PdfToOffice/GgomiLabUnitTest/TestCommands.cs
@@ -0,0 +1,30 @@
+﻿using GgomiLab;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace GgomiLabUnitTest
+{
+    [TestClass]
+    public class TestCommands
+    {
+        [TestMethod]
+        public void TestAddDoc()
+        {
+            DocListBox docListBox = new DocListBox();
+            docListBox.BeginInit();
+            docListBox.ItemsSource = new ObservableCollection<Doc>();
+            docListBox.EndInit();
+
+            string[] fileNames = { "a.pdf", "b.pdf", "c.pdf" };
+            DocListBox.AddDoc.Execute(fileNames, docListBox);
+
+            var items = docListBox.ItemsSource as IList<Doc>;
+            Assert.AreEqual(fileNames.Length, items.Count);
+            for (int i = 0; i < fileNames.Length; i++)
+            {
+                Assert.AreEqual(fileNames[i], items[i].FilePath);
+            }
+        }
+    }
+}
