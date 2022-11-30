e7892081f580d54ba3234f64f4b176d44989da65
hyunhee2 <117071412+hyunhee2@users.noreply.github.com>
2022-11-30 15:50:32 +0900

    #44 UI 쓰레드와 변환 쓰레드 분리 (#47)
    * #44 UI 쓰레드와 변환 쓰레드 분리
    
    [내용]
    - BackgroundWorker를 사용하여 UI Thread와 변환 Thread 분리
    - DoWork 결과를 RunWorkerCompleted에 전달 후 MsgBox 도시
    - 프로그램 초기화 시 실패할 경우 예외 처리
    - 메시지 리소스 추가
    - TODO 추가 : 변환 실패 시 아이콘으로 표시
    
    [영향]
    - 변환 작업 수행 시 UI 사용 가능
    - 프로그램 초기화 실패 시 에러메시지 도시
    - 변환 시 취소, 완료, 실패 메시지
    - 변환 실패 시 각각 뜨던 메시지 삭제

--- PdfToOfficeApp/MainWindow.xaml.cs
+++ PdfToOfficeApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -13,6 +14,7 @@ namespace PdfToOfficeApp
     public partial class MainWindow : Window, IDisposable
     {
         private PdfToOfficeProxy pdfToOffice;
+        private BackgroundWorker worker = new BackgroundWorker();
 
         public MainWindow()
         {
@@ -27,6 +29,12 @@ namespace PdfToOfficeApp
                 pdfToOffice = null;
             }
 
+            if(worker != null)
+            {
+                worker.Dispose();
+                worker = null;
+            }
+
             ContentRendered -= MainWindow_ContentRendered;
         }
 
@@ -38,7 +46,78 @@ namespace PdfToOfficeApp
             ContentRendered += MainWindow_ContentRendered;
 
             pdfToOffice = new PdfToOfficeProxy();
-            pdfToOffice.InitializeSolidFramework(); // TODO : 초기화 실패 시 예외처리 해야 함(ErrorStatus)
+            ErrorStatus result = pdfToOffice.InitializeSolidFramework();
+            if (result != ErrorStatus.Success)
+            {
+                if (GetModel().ShowMsg)
+                {
+                    string strErr = string.Format("초기화를 실패하였습니다.\nError Code : {0}", result.ToString());
+                    MessageBox.Show(strErr);
+                    return;
+                }
+            }
+
+            worker.DoWork += Worker_DoWork;
+            worker.RunWorkerCompleted += Worker_RunWorkerCompleted;
+            worker.ProgressChanged += Worker_ProgressChanged;
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+        }
+
+        private void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
+        }
+
+        // 작업 완료
+        private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            string strResult;
+            if (e.Error != null)
+            {
+                strResult = e.Error.Message;
+            }
+            else if (e.Cancelled)
+            {
+                strResult = Util.String.GetMsg(ErrorStatus.Canceled);
+            }
+            else if ((int)e.Result > 0)
+            {
+                strResult = (int)e.Result + "개 파일 " + Util.String.GetMsg(ErrorStatus.Fail);
+            }
+            else
+            {
+                strResult = Util.String.GetMsg(ErrorStatus.Success);
+            }
+
+            if (GetModel().ShowMsg)
+            {
+                MessageBox.Show(strResult);
+            }
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int failCount = 0;
+
+            foreach (Doc doc in (DocList)e.Argument)
+            {
+                if (worker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+
+                string path = doc.FilePath;
+                ErrorStatus status = pdfToOffice.DoWordConversion(path, "");
+                if (status != ErrorStatus.Success)
+                {
+                    failCount++;
+                    // TODO : 변환 실패 항목은 추후 아이콘으로 표시
+                }
+            }
+
+            e.Result = failCount;
         }
 
         private void AddCommandHandlers()
@@ -128,24 +207,8 @@ namespace PdfToOfficeApp
 
         private void OnConvert(object sender, ExecutedRoutedEventArgs e)
         {
-            int failCount = 0;
-            foreach (Doc doc in GetModel().Docs)
-            {
-                string path = doc.FilePath;
-                ErrorStatus status = pdfToOffice.DoWordConversion(path, "");
-                if (status != ErrorStatus.Success)
-                {
-                    failCount++;
-                    if (GetModel().ShowMsg)
-                        MessageBox.Show(Util.String.GetMsg(status));
-                }
-            }
-
-            if (failCount == 0)
-            {
-                if (GetModel().ShowMsg)
-                    MessageBox.Show(Util.String.GetMsg(ErrorStatus.Success));
-            }
+            DocList docList = GetModel().Docs;
+            worker.RunWorkerAsync(docList);
         }
 
         private void CanConvert(object sender, CanExecuteRoutedEventArgs e)
@@ -164,8 +227,7 @@ namespace PdfToOfficeApp
         // 파일 추가
         private void OnAddFile(object sender, ExecutedRoutedEventArgs e)
         {
-            string[] fileNames = e.Parameter as string[];
-            if (fileNames == null)
+            if (!(e.Parameter is string[] fileNames))
             {
                 return;
             }
